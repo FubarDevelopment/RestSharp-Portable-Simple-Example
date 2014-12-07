00c6c600935ea7d35fa3f1e4c0161c868a60a33e
Mark Junker <mark.junker75@gmail.com>
2014-12-07 23:46:17 +0100

    First version

---
+++ FubarDev.Example.GoogleMaps.Android/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace FubarDev.Example.GoogleMaps.Android
+{
+    [Activity(Label = "FubarDev.Example.GoogleMaps.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.buttonSearch);
+            button.Click += button_Click;
+        }
+
+        async void button_Click(object sender, EventArgs e)
+        {
+
+            var Origin = FindViewById<EditText>(Resource.Id.editOrigin);
+            var Destination = FindViewById<EditText>(Resource.Id.editDestination);
+            var OutputText = FindViewById<EditText>(Resource.Id.editOutput);
+            try
+            {
+                var client = new GoogleMapsClient();
+                var result = await client.GetDirections(Origin.Text, Destination.Text);
+                OutputText.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                new AlertDialog.Builder(this)
+                    .SetMessage(ex.ToString())
+                    .SetTitle("Error")
+                    .Show();
+            }
+        }
+    }
+}
+
---
+++ FubarDev.Example.GoogleMaps/GoogleMapsClient.cs
@@ -0,0 +1,44 @@
+﻿using RestSharp.Portable;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FubarDev.Example.GoogleMaps
+{
+    public class GoogleMapsClient
+    {
+        private readonly RestClient _client;
+
+        public CultureInfo Language { get; set; }
+
+        public GoogleMapsClient(IWebProxy proxy = null)
+        {
+            Language = CultureInfo.CurrentCulture;
+            _client = new RestClient("https://maps.googleapis.com/maps/api")
+            {
+                Proxy = proxy,
+            };
+        }
+
+        public async Task<dynamic> GetDirections(string origin, string destination)
+        {
+            var request = new RestRequest("directions/{output}");
+            request.AddUrlSegment("output", "json");
+            request.AddParameter("language", Language.TwoLetterISOLanguageName);
+            request.AddObject(new
+            {
+                sensor = false,
+                origin = origin,
+                destination = destination,
+            });
+            var response = await _client.Execute<dynamic>(request);
+            var data = response.Data;
+            return data;
+        }
+    }
+}
